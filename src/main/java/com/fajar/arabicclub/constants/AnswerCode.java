2e85c94116dba0e2b15800825f9b3a1ca27be7f1
Fajar AM <somabangsa@gmail.com>
2021-02-13 10:07:07 +0700

    update config

--- src/main/java/com/fajar/arabicclub/constants/AnswerCode.java
+++ src/main/java/com/fajar/arabicclub/constants/AnswerCode.java
@@ -3,8 +3,20 @@ package com.fajar.arabicclub.constants;
 public enum AnswerCode {
 	A, B,C, D, E;
 
+	static boolean isExist(String value) {
+		AnswerCode[] values = AnswerCode.values();
+		for (AnswerCode answerCode : values) {
+			if (answerCode.toString().equals(value)) {
+				return true;
+			}
+		}
+		return false;
+	}
 	public static AnswerCode parse(String rawAnswerCode) {
 		try {
+			if (false == isExist(rawAnswerCode))  {
+				return null;
+			}
 			return valueOf(rawAnswerCode);
 		} catch (Exception e) { 
 		}
